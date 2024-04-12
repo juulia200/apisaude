197eb03cca7687ab117f23c3a3cbf66f3acbcd08
julia <julia.oliveira25@portalsesisp.org.br>
2024-04-12 11:12:07 -0300

    apisaude

---
+++ Models/Paciente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace apisaude.Models;
+
+public partial class Paciente
+{
+    public int CodPaciente { get; set; }
+
+    public long Cpf { get; set; }
+
+    public string NomePaciente { get; set; } = null!;
+
+    public long? CartaoSus { get; set; }
+
+    public string? Rua { get; set; }
+
+    public int? NumeroCasa { get; set; }
+
+    public string? Bairro { get; set; }
+
+    public string? Cidade { get; set; }
+
+    public long? Telefone { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public int? CodMed { get; set; }
+    [JsonIgnore]
+
+    public virtual Medico? CodMedNavigation { get; set; }
+
+    public virtual ICollection<Entrega> Entregas { get; } = new List<Entrega>();
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using apisaude.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var MyAllowSpecificOrigins = "_MyAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(MyAllowSpecificOrigins, PolicyServiceCollectionExtensions =>{
+        PolicyServiceCollectionExtensions.WithOrigins("*").AllowAnyHeader().AllowAnyHeader();
+
+});
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
